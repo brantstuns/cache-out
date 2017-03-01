3cf60adf532a55fbd2fab917fef0ad056612f91d
Brant Stuns <brantstuns@gmail.com>
2017-03-01 00:48:26 -0700

    Added more tests and fixed indentation in travis yml

--- tests/indexTest.js
+++ tests/indexTest.js
@@ -10,14 +10,21 @@ sinonStubPromise(sinon);
 
 describe('cache-out ', function() {
   beforeEach(function() {
+    this.redisOptions = {port: 1111, host: 'localhost', db: '0', password: ''};
+
     this.redisClientStub = sinon.stub();
     this.redisClientStub.prototype.get = sinon.stub();
     this.redisClientStub.prototype.setex = sinon.stub();
+
+    this.consoleStub = {log: sinon.stub()}; // I know, I know... I just hate seeing it in the shell
+
     this.unwireIndex = index.__set__('redis', this.redisClientStub);
+    this.unwireConsole = index.__set__('console', this.consoleStub);
   });
 
   afterEach(function() {
     this.unwireIndex();
+    this.unwireConsole();
   });
 
   describe('and the request is a GET ', function() {
@@ -31,12 +38,38 @@ describe('cache-out ', function() {
 
     it('it should call get on the redisClient with the endpoint', function (done) {
       this.redisClientStub.prototype.get.returnsPromise().resolves(`{"nice": "cached response"}`);
-      index(this.requestClientStub, this.requestOptions,
-        {port: 1111, host: 'localhost', db: '0', password: ''})
+      index(this.requestClientStub, this.requestOptions, this.redisOptions)
         .then(() => {
           expect(this.redisClientStub.prototype.get).to.have.been.calledWith(this.requestOptions.url);
           done();
         });
+    });
+
+    describe('and something throws an error ', function () {
+      it('it should just make the request and return the response', function (done) {
+        this.redisClientStub.prototype.get.returnsPromise().rejects('the worst error ever');
+        this.requestClientStub.returnsPromise().resolves(`{"nice": "service response when redis broke"}`);
+
+        index(this.requestClientStub, this.requestOptions, this.redisOptions)
+          .then(() => {
+            expect(this.redisClientStub.prototype.get).to.have.been.calledWith(this.requestOptions.url);
+            done();
+          });
+      });
+    });
+
+    describe('and there is no cached response in redis ', function () {
+      it('it should call the passed request with the passed requestOptions', function (done) {
+        this.redisClientStub.prototype.get.returnsPromise().resolves('');
+        this.requestClientStub.returnsPromise().resolves(`{"nice": "service response"}`);
+        this.redisClientStub.prototype.setex.returnsPromise().resolves('cached response in redis');
+
+        index(this.requestClientStub, this.requestOptions, this.redisOptions)
+          .then(() => {
+            expect(this.requestClientStub).to.have.been.calledWith(this.requestOptions);
+            done();
+          });
+      });
     });
   });
 });
